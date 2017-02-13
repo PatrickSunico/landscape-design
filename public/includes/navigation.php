4854a0649994a45276846611eb615dc93de56ebc
PatrickSunico <apatricksunico@gmail.com>
2017-02-13 20:30:14 +0800

    Update 8

--- public/includes/navigation.php
+++ public/includes/navigation.php
@@ -1,12 +1,18 @@
 <nav>
- <div class="nav-custom">
-    <span class="logo">
-      <a class="logo-brand" href="#"> XYZ </a>
-    </span>
-    <a href="#" class="hamburger">
-      <div class="bar1"></div>
-      <div class="bar2"></div>
-      <div class="bar3"></div>
-    </a>
-  </div>
+  <span class="logo">
+    <a class="logo-brand" href="#"> XYZ </a>
+  </span>
+  <ul class="nav-wrapper">
+    <!-- if you want to add more links uncomment this line  -->
+    <!-- <li>
+      <a href="#">Link 1</a>
+    </li> -->
+    <li>
+      <a href="#" class="hamburger">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </a>
+    </li>
+  </ul>
 </nav>
